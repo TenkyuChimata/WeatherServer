39033cc05212e1652f80b6e6ccceea684c7ad885
TenkyuChimata <tenkyuchimata@gmail.com>
2025-03-24 09:21:36 +0800

    Formatting

--- air_data.py
+++ air_data.py
@@ -8,7 +8,8 @@ import collections
 from sds011 import SDS011
 
 pm10, pm25 = 0.1, 0.1
-usv_list = collections.deque(maxlen = 60)
+usv_list = collections.deque(maxlen=60)
+
 
 def avg(arr):
     if len(arr) == 0:
@@ -16,34 +17,36 @@ def avg(arr):
     average = sum(arr) / len(arr)
     return average
 
+
 def sds011():
     global pm10, pm25
-    sds = SDS011("/dev/ttyUSB0", use_query_mode = True)
-    sds.sleep(sleep = False)
+    sds = SDS011("/dev/ttyUSB0", use_query_mode=True)
+    sds.sleep(sleep=False)
     while True:
         try:
             time.sleep(20)
             sds_data = sds.query()
             if sds_data[0] > 0:
-               pm25 = sds_data[0]
+                pm25 = sds_data[0]
             else:
-               pm25 = 10.0
+                pm25 = 10.0
             if sds_data[1] > 0:
-               pm10 = sds_data[1]
+                pm10 = sds_data[1]
             else:
-               pm10 = 10.0
+                pm10 = 10.0
             sds.sleep()
             time.sleep(40)
-            sds.sleep(sleep = False)
+            sds.sleep(sleep=False)
         except Exception as e:
             print(f"{datetime.datetime.now().strftime('[%H:%M:%S]')} Error: {e}")
             time.sleep(1)
             continue
 
+
 def main():
     while True:
         try:
-            esp8266_data = requests.get("http://192.168.0.137", timeout = 5).json()
+            esp8266_data = requests.get("http://192.168.0.137", timeout=5).json()
             usv_list.append(esp8266_data["usv"])
             data = {
                 "temperature": esp8266_data["temperature"],
@@ -53,9 +56,9 @@ def main():
                 "pm10": pm10,
                 "usv": esp8266_data["usv"],
                 "usv_avg": avg(usv_list),
-                "create_at": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                "create_at": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             }
-            with open("/var/www/html/data.json", "w", encoding = "utf-8") as f:
+            with open("/var/www/html/data.json", "w", encoding="utf-8") as f:
                 json.dump(data, f)
             time.sleep(60)
         except Exception as e:
@@ -63,7 +66,8 @@ def main():
             time.sleep(1)
             continue
 
-thread1 = threading.Thread(target = sds011)
-thread2 = threading.Thread(target = main)
+
+thread1 = threading.Thread(target=sds011)
+thread2 = threading.Thread(target=main)
 thread1.start()
 thread2.start()
--- plot.py
+++ plot.py
@@ -6,18 +6,19 @@ import collections
 from pyecharts.charts import Line, Page
 from pyecharts import options as opts
 
-temperature_list = collections.deque(maxlen = 288)
-humidity_list = collections.deque(maxlen = 288)
-pressure_list = collections.deque(maxlen = 288)
-radiation_list = collections.deque(maxlen = 288)
-radiation_avg_list = collections.deque(maxlen = 288)
-pm25_list = collections.deque(maxlen = 288)
-pm10_list = collections.deque(maxlen = 288)
-createat_list = collections.deque(maxlen = 288)
+temperature_list = collections.deque(maxlen=288)
+humidity_list = collections.deque(maxlen=288)
+pressure_list = collections.deque(maxlen=288)
+radiation_list = collections.deque(maxlen=288)
+radiation_avg_list = collections.deque(maxlen=288)
+pm25_list = collections.deque(maxlen=288)
+pm10_list = collections.deque(maxlen=288)
+createat_list = collections.deque(maxlen=288)
+
 
 def get_data():
     try:
-        data = requests.get("http://127.0.0.1/data.json", timeout = 3).json()
+        data = requests.get("http://127.0.0.1/data.json", timeout=3).json()
         radiation = data["usv"]
         radiation_avg = data["usv_avg"]
         temperature = data["temperature"]
@@ -29,18 +30,35 @@ def get_data():
     except Exception as e:
         print(f"{datetime.datetime.now().strftime('[%H:%M:%S]')} Error: {e}")
         return None
-    return temperature, humidity, pressure, radiation, radiation_avg, pm25, pm10, create_at
+    return (
+        temperature,
+        humidity,
+        pressure,
+        radiation,
+        radiation_avg,
+        pm25,
+        pm10,
+        create_at,
+    )
+
 
 def plot(x, y, y_name, plot_name, html_name):
     line = (
-        Line(init_opts = opts.InitOpts(width = "100%", height = "815px"))
+        Line(init_opts=opts.InitOpts(width="100%", height="815px"))
         .add_xaxis(x)
-        .add_yaxis(y_name, y, is_smooth = True, label_opts = opts.LabelOpts(is_show = False))
-        .set_global_opts(title_opts = opts.TitleOpts(title = plot_name), xaxis_opts = opts.AxisOpts(axislabel_opts = opts.LabelOpts(rotate = 90, interval = 0, is_show = False)), yaxis_opts = opts.AxisOpts(is_scale = True))
+        .add_yaxis(y_name, y, is_smooth=True, label_opts=opts.LabelOpts(is_show=False))
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title=plot_name),
+            xaxis_opts=opts.AxisOpts(
+                axislabel_opts=opts.LabelOpts(rotate=90, interval=0, is_show=False)
+            ),
+            yaxis_opts=opts.AxisOpts(is_scale=True),
+        )
     )
-    page = Page(layout = Page.SimplePageLayout, page_title = plot_name)
+    page = Page(layout=Page.SimplePageLayout, page_title=plot_name)
     page.add(line)
     page.render(html_name)
+
 
 while True:
     try:
@@ -54,13 +72,55 @@ while True:
             pm25_list.append(weather_data[5])
             pm10_list.append(weather_data[6])
             createat_list.append(weather_data[7])
-            plot(list(createat_list), list(temperature_list), "温度 (℃)", "温度", "/var/www/html/temperature.html")
-            plot(list(createat_list), list(humidity_list), "湿度 (%RH)", "湿度", "/var/www/html/humidity.html")
-            plot(list(createat_list), list(pressure_list), "大气压 (hPa)", "大气压", "/var/www/html/pressure.html")
-            plot(list(createat_list), list(radiation_list), "电离辐射 (μSv/h)", "电离辐射", "/var/www/html/radiation.html")
-            plot(list(createat_list), list(radiation_avg_list), "电离辐射 (μSv/h)", "电离辐射(小时均值)", "/var/www/html/radiation_avg.html")
-            plot(list(createat_list), list(pm25_list), "PM2.5 (μg/m³)", "PM2.5", "/var/www/html/pm2.5.html")
-            plot(list(createat_list), list(pm10_list), "PM10 (μg/m³)", "PM10", "/var/www/html/pm10.html")
+            plot(
+                list(createat_list),
+                list(temperature_list),
+                "温度 (℃)",
+                "温度",
+                "/var/www/html/temperature.html",
+            )
+            plot(
+                list(createat_list),
+                list(humidity_list),
+                "湿度 (%RH)",
+                "湿度",
+                "/var/www/html/humidity.html",
+            )
+            plot(
+                list(createat_list),
+                list(pressure_list),
+                "大气压 (hPa)",
+                "大气压",
+                "/var/www/html/pressure.html",
+            )
+            plot(
+                list(createat_list),
+                list(radiation_list),
+                "电离辐射 (μSv/h)",
+                "电离辐射",
+                "/var/www/html/radiation.html",
+            )
+            plot(
+                list(createat_list),
+                list(radiation_avg_list),
+                "电离辐射 (μSv/h)",
+                "电离辐射(小时均值)",
+                "/var/www/html/radiation_avg.html",
+            )
+            plot(
+                list(createat_list),
+                list(pm25_list),
+                "PM2.5 (μg/m³)",
+                "PM2.5",
+                "/var/www/html/pm2.5.html",
+            )
+            plot(
+                list(createat_list),
+                list(pm10_list),
+                "PM10 (μg/m³)",
+                "PM10",
+                "/var/www/html/pm10.html",
+            )
             time.sleep(300)
         else:
             time.sleep(5)
